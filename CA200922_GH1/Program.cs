5a5ec425d4b7f0a52a15334c57ef695814fceed2
JuhaszZoltan <juhaszzoltanmate@yahoo.com>
2020-09-22 08:58:33 +0200

    Add project files.

---
+++ CA200922_GH1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CA200922_GH1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Juhász Zoltán");
+            Console.ReadKey();
+        }
+    }
+}
